fb064caea326a4e6bdc53025d49bb6a663dcfb29
Godfred Adjei-Darko <gadjeid@siue.edu>
2024-01-24 08:51:59 -0600

    adding an additional line to hello_world.py for feature1

--- hello_world.py
+++ hello_world.py
@@ -1,7 +1,7 @@
 def introduction(name):
-    message = f"Hello, my name is {name} and this is for CS325."
+    message = f"Hello, my name is {name} and this is for CS325.\n"
+    message += "This is our first programming assignment in CS325 using git and python.\n"
     print(message)
-
 
 user_name = "Godfred Adjei-Darko"
 introduction(user_name)
